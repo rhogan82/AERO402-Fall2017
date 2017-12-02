4a1fa8f0929f9c0b5e7565c914c55a8c64a04e80
nickrbe <nickrbe@tamu.edu>
2017-12-02 12:45:22 -0600

    Add files via upload
    Format changes to adhere to class standards

--- 402_Integration/ROB/Robonaut.h
+++ 402_Integration/ROB/Robonaut.h
@@ -1,11 +1,52 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/*************************************************************************
+**********************(COPYRIGHT INFORMATION)*****************************
+**************************************************************************/
 
+/*************************************************************************
+PURPOSE:
+This class performs path planning calculations, and then sets the new location for the robot/actor using the algorithim
+
+LANGUAGE:
+(C++)
+LIBRARY DEPENDENCIES:
+(
+#include "CoreMinimal.h" // Unreal Library
+#include "Robonaut.generated.h" //Actor applied to
+#include <math.h>	// for calculations
+)
+REFERENCES:
+(
+//References: A potential Field approach to path planning, Hwang and Ahuja
+// https://wiki.unrealengine.com/Iterators:_Object_%26_Actor_Iterators,_Optional_Class_Scope_For_Faster_Search
+)
+ASSUMPTIONS AND LIMITATIONS:
+(
+Assumption/Limitation 1: The robot always can always sense and locate an object or obstacle.
+For this code, the object detection sub model was unavailable, so the object locations are known and hard coded in.
+
+Assumption 2: Given a new coordinate to move to, the object (robot) can perform all the necessary translations and rotations to move itself to the new l
+location.
+
+Assumption 3: The robot receives an end point/goal to travel to, and is able to precisely determine its relative position to that of the end point.
+
+)
+PROGRAMMERS:
+(
+((Nicholas Beal) (Robotics and Teleoperation/Team 9) (12/1) (Removed the Unreal Detection submodel from code, new position is now returned to any actor that calls this math model))
+)
+**************************************************************************/
+
+// NOTE: Format changes and a couple of bugs are still being worked on.
+/*Include this in all header files*/
+#pragma once
+
+/*includes necessary for the header file. Minimize these if possible*/
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Robonaut.generated.h"
-#include <math.h>
+
 UCLASS()
 class ROBONAUTTEST_API ARobonaut : public AActor
 {
@@ -32,7 +73,7 @@ private:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-	float RunningTime;
+	double RunningTime;
 	
 	
 };
